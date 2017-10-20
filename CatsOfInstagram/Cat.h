e91bb946571691580ba06e91d344944ffc931272
olga-nest <olga.w.nesterova@gmail.com>
2017-10-19 19:20:07 -0700

    Add images

---
+++ CatsOfInstagram/Cat.h
@@ -0,0 +1,13 @@
+//
+//  Cat.h
+//  CatsOfInstagram
+//
+//  Created by Olga on 10/19/17.
+//  Copyright © 2017 Olga Nesterova. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Cat : NSObject
+
+@end
